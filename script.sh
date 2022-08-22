fb04d0602ff53a79ca4c3f94b6d275fba73294b1
DevOps Project <92192934+jeet94singh@users.noreply.github.com>
2022-08-22 11:55:49 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+echo 1. what is in this directory?
+ls -a
+echo 2. Is java installed?
+java -version
+echo 3. Is git installed?
+git --version
+echo 4. what is the workspace location?
+echo $RUNNER_WORKSPACE
+echo
+echo 5. who is running this script?
+whoami
+echo 6. How is the disc laid out?
+df
+echo 7. what environment variable are avaialble?
+env
